e00c9358819f19c979edfe3a00130593195be2bc
vtphan <vphan@memphis.edu>
2018-11-18 14:10:17 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This program is intended to be useful when you want to estimate how much RAM
+is needed for a system to have little memory pressure, i.e. without memory
+compression on a MacOS system.
+
+The program computes the total amount of RAM of all programs being run on a
+MacOS system.  Total RAM is not the same as "memory usage", displayed by
+Activity Monitor.  Due to memory compression, memory usage is always less
+than the system memory.  Memory usage is, therefore, not the total of RAM
+of all programs being run.
+
---
+++ total_mem.py
@@ -0,0 +1,44 @@
+'''
+This computes the total amount of RAM of all programs being run on a MacOS system.
+This is not the same as "memory usage", displayed by Activity Monitor.
+Due to memory compression, memory usage is always less than the system memory.
+Memory usage is, therefore, not the total of RAM of all programs being run.
+'''
+import subprocess
+
+command = ['top', '-o', 'mem', '-stats', 'mem,command', '-i', '5']
+t = 1
+
+print('Gathering mem info...')
+with open('mem.txt', 'w') as output:
+	try:
+		subprocess.run(command, timeout=t, stdout=output)
+	except subprocess.TimeoutExpired:
+		pass
+	except:
+		print('Error executing command.')
+
+print('Analyzing mem info...')
+with open('mem.txt') as input:
+	for line in input:
+		if line.startswith('MEM'):
+			break
+	total = 0
+	oneK = 1024
+	oneM = oneK * 1024
+	oneG = oneM * 1024
+	for line in input:
+		mem, command = line.strip().split(' ', 1)
+		mem = mem.strip('\n\t +-')
+		if mem.endswith('G'):
+			mem += int(mem[:-1]) * oneG
+		elif mem.endswith('M'):
+			total += int(mem[:-1]) * oneM
+		elif mem.endswith('K'):
+			total += int(mem[:-1]) * oneK
+		else:
+			total += int(mem)
+		command = command.strip() + ' ' * 20
+		print('{}\t{}\t{}G'.format(command[:20], mem, round(total/oneG,2)))
+	print('Total memory usage: {}G'.format(round(total/oneG,2)))
+
